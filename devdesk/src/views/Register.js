f141cc16d32a6b833f109522c3fe535c98e0a858
raythurman2386 <raymondthurman5@gmail.com>
2019-09-18 21:36:16 -0400

    adding imports for the register page

---
+++ devdesk/src/views/Register.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+// Material UI components
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+
+const Register = () => {
+  return <form></form>
+}
+
+export default Register
