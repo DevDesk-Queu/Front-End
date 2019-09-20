8909be51d5631fa199db285f5165b3336d479ea7
raythurman2386 <raymondthurman5@gmail.com>
2019-09-19 21:27:12 -0400

    login and register routes set up in app

--- devdesk/src/App.js
+++ devdesk/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+
+// Views
+import Login from './views/Login'
+import Register from './views/Register'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          DevDesk Queu
-        </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Switch>
+        <Route path='/login' render={props => <Login {...props} />} />
+        <Route path='/register' render={props => <Register {...props} />} />
+      </Switch>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
