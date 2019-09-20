78595bc07afe240d52f3b6025846ef54b535b390
raythurman2386 <raymondthurman5@gmail.com>
2019-09-19 21:03:41 -0400

    added withAuth config

--- devdesk/src/utils/axiosConfig.js
+++ devdesk/src/utils/axiosConfig.js
@@ -5,3 +5,13 @@ export const axiosWithoutAuth = () => {
     baseURL: 'https://devdeskbackend.herokuapp.com/api',
   })
 }
+
+export const axiosWithAuth = token => {
+  return axios.create({
+    baseURL: 'https://devdeskbackend.herokuapp.com/api',
+    headers: {
+      Authorization: token,
+      'Content-Type': 'application/json',
+    },
+  })
+}
