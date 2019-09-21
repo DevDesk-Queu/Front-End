4b2aa8d2f7b3ab892d8f7d639f2a0ca899e7f9a9
mandihamza <mandihamza@gmail.com>
2019-09-21 13:48:16 -0600

    Adds CssBaseline as a global component

--- devdesk/src/index.js
+++ devdesk/src/index.js
@@ -4,8 +4,11 @@ import './index.css'
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom'
 
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 ReactDOM.render(
   <Router>
+    <CssBaseline />
     <App />
   </Router>,
   document.getElementById('root'),
