f4d6afe402e56a0e04bde0a904e6eabe3b34b8f1
raythurman2386 <raymondthurman5@gmail.com>
2019-09-22 00:10:55 -0400

    removed excess files till talking with the group

---
+++ devdesk/src/components/Nav/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useSpring, animated, config } from 'react-spring'
+
+// Components
+import Brand from './Brand'
+import BurgerMenu from './BurgerMenu'
+import CollapseMenu from './CollapseMenu'
+
+const Navbar = () => {
+  return <div></div>
+}
+
+export default Navbar
