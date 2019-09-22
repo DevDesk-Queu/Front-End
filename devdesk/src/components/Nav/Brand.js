ab4176ac4b0f66c7e5422e572574d31bc27b4c58
raythurman2386 <raymondthurman5@gmail.com>
2019-09-21 23:30:15 -0400

    added react spring and nav files

---
+++ devdesk/src/components/Nav/Brand.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Brand = () => {
+  return <img style={imgStyles} alt='DevDesk' />
+}
+
+export default Brand
+
+const imgStyles = {
+  height: '85%',
+  margin: 'auto 0',
+}
