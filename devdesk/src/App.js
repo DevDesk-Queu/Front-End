801467bac5d9e4a13e1f1edcafa311deaae87319
stev <ss@stev.ee>
2019-09-23 00:40:30 -0700

    Add new ticket form including api functionality

--- devdesk/src/App.js
+++ devdesk/src/App.js
@@ -4,13 +4,20 @@ import { Route, Switch } from 'react-router-dom'
 // Views
 import Login from './views/Login'
 import Register from './views/Register'
+import NewTicket from './components/NewTicket'
 
 function App() {
+
+  //example props to be passed down
+  const user_id = 5
+  const categories = ['category1', 'category2', 'category3', 'category4', 'category5']
+
   return (
     <div className='App'>
       <Switch>
         <Route path='/login' render={props => <Login {...props} />} />
         <Route path='/register' render={props => <Register {...props} />} />
+        <Route path='/newticket' render={props => <NewTicket {...props} user_id={user_id} categories={categories}/>} />
       </Switch>
     </div>
   )
