f401b88b7e2df05c734ef623bdb90f747bee3d60
mandihamza <mandihamza@gmail.com>
2019-09-23 18:13:40 -0600

    Initial commit for student dashboard

--- devdesk/src/App.js
+++ devdesk/src/App.js
@@ -5,6 +5,8 @@ import { Route, Switch } from 'react-router-dom'
 import Login from './views/Login'
 import Register from './views/Register'
 import PrivateRoute from './components/PrivateRoute'
+import StudentDashboard from './views/StudentDashboard'
+
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
         <Route path='/login' render={props => <Login {...props} />} />
         <Route path='/register' render={props => <Register {...props} />} />
         <PrivateRoute path='/dashboard' component={Register} />
+        <Route path='/studentdashboard' render={props => <StudentDashboard {...props} />} />
         <Route component={Login} />
       </Switch>
     </div>
