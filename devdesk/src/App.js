643b680066e8e28226dae54121aee833b72d3fe4
raythurman2386 <raymondthurman5@gmail.com>
2019-09-24 20:24:36 -0400

    Updated imports, adding HelperRoute

--- devdesk/src/App.js
+++ devdesk/src/App.js
@@ -5,23 +5,40 @@ import { Route, Switch } from 'react-router-dom'
 import Login from './views/Login'
 import Register from './views/Register'
 import NewTicket from './components/NewTicket'
-import PrivateRoute from './components/PrivateRoute'
+import StudentRoute from './components/StudentRoute'
 import StudentDashboard from './views/StudentDashboard'
+import HelperDashboard from './views/HelperDashboard'
 
 function App() {
-
   //example props to be passed down
   const user_id = 5
-  const categories = ['category1', 'category2', 'category3', 'category4', 'category5']
+  const categories = [
+    'category1',
+    'category2',
+    'category3',
+    'category4',
+    'category5',
+  ]
 
   return (
     <div className='App'>
       <Switch>
         <Route path='/login' render={props => <Login {...props} />} />
         <Route path='/register' render={props => <Register {...props} />} />
-        <Route path='/newticket' render={props => <NewTicket {...props} user_id={user_id} categories={categories}/>} />
-        <Route path='/studentdashboard' render={props => <StudentDashboard {...props} />} />
-        <PrivateRoute path='/dashboard' render={props => <StudentDashboard {...props} />} />
+        <Route
+          path='/newticket'
+          render={props => (
+            <NewTicket {...props} user_id={user_id} categories={categories} />
+          )}
+        />
+        <StudentRoute
+          path='/studentdashboard'
+          render={props => <StudentDashboard {...props} />}
+        />
+        <HelperRoute
+          path='/helperdashboard'
+          render={props => <HelperDashboard {...props} />}
+        />
         <Route component={Login} />
       </Switch>
     </div>
