8e6abe158631c51340f7288a032b081e910d2823
raythurman2386 <raymondthurman5@gmail.com>
2019-09-26 19:34:54 -0400

    adding formik to register

--- devdesk/src/views/Register.js
+++ devdesk/src/views/Register.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useRef } from 'react'
 import { axiosWithoutAuth as axios } from '../utils/axiosConfig'
 import { Link } from 'react-router-dom'
+import { Form, Field, withFormik } from 'formik'
+import * as Yup from 'yup'
 
 // Form Components
 import Avatar from '@material-ui/core/Avatar'
@@ -16,6 +18,7 @@ import InputLabel from '@material-ui/core/InputLabel'
 import FormControl from '@material-ui/core/FormControl'
 import Select from '@material-ui/core/Select'
 import MenuItem from '@material-ui/core/MenuItem'
+import { TextField } from 'formik-material-ui'
 
 const Register = props => {
   // console.log(props)
