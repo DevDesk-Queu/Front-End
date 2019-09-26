b018f4905fcd20de8ba9a048d302cdd78f64ed80
raythurman2386 <raymondthurman5@gmail.com>
2019-09-25 21:36:57 -0400

    minor style changes

--- devdesk/src/views/HelperDashboard.js
+++ devdesk/src/views/HelperDashboard.js
@@ -4,9 +4,15 @@ import { axiosWithAuth as axios } from '../utils/axiosConfig'
 import OpenTicket from '../components/OpenTicket'
 import Card from '@material-ui/core/Card'
 import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { makeStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
 
 export default function StudentDashboard() {
   const [tickets, setTickets] = useState([])
+
+  // styles
+  const classes = useStyles()
 
   useEffect(() => {
     getAllTickets()
@@ -23,11 +29,24 @@ export default function StudentDashboard() {
       })
   }
 
+  const getMyTickets = e => {
+    // axios()
+    //   .get(`/users/${}/tickets`)
+    //   .then(res => {
+    //     setTickets(res.data)
+    //   })
+    //   .catch(err => {
+    //     console.log('err', err)
+    //   })
+    console.log('clicked')
+  }
+
   return (
-    <section>
+    <Container component='main' maxWidth='xl'>
+      <CssBaseline />
       <Button onClick={e => getAllTickets(e)}>All Tickets</Button>
-      <Button>My Tickets</Button>
-      <Card>
+      <Button onClick={e => getMyTickets(e)}>My Tickets</Button>
+      <Card className={classes.paper}>
         {tickets &&
           tickets.map(ticket => {
             return (
@@ -44,6 +63,24 @@ export default function StudentDashboard() {
             )
           })}
       </Card>
-    </section>
+    </Container>
   )
 }
+
+// Set style theme for the form components
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white,
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+  },
+  buttons: {
+    display: 'flex',
+  },
+}))
