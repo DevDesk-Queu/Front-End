4fb2d33d586592370989820ab30d5e7c9845c83e
mandihamza <mandihamza@gmail.com>
2019-09-29 19:02:20 -0600

    Updates styles for open tickets

--- devdesk/src/components/OpenTicket.js
+++ devdesk/src/components/OpenTicket.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { axiosWithAuth as axios } from '../utils/axiosConfig'
 
 import { makeStyles } from '@material-ui/core/styles'
-import Container from '@material-ui/core/Container'
+import clsx from 'clsx';
 import Grid from '@material-ui/core/Grid'
 import Card from '@material-ui/core/Card'
 import Typography from '@material-ui/core/Typography'
@@ -12,11 +12,146 @@ import Divider from '@material-ui/core/Divider'
 import Avatar from '@material-ui/core/Avatar'
 import Button from '@material-ui/core/Button'
 import CardActions from '@material-ui/core/CardActions'
+import Box from '@material-ui/core/Box'
+import IconButton from '@material-ui/core/IconButton';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Collapse from '@material-ui/core/Collapse';
+import { green } from '@material-ui/core/colors/green';
+
+const user_id = localStorage.getItem('user_id')
+const ticket_id = localStorage.getItem('ticket_id')
+
+export default function OpenTicket() {
+  const classes = useStyles()
+  const [tickets, setTickets] = useState([])
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  useEffect(() => {
+    getAllTickets()
+  }, [])
+
+  // ********** To test replace ${user_id} with a user id from DB 1, 2, 3 or 4 ********** 
+  const getAllTickets = () => {
+    axios()
+      .get(`users/1/tickets`)
+      .then(res => {
+        setTickets(res.data)
+      })
+      .catch(err => {
+        console.log('err', err)
+      })
+  }
+
+// Delete ticket
+    const deleteTicket = () => {
+        axios()
+            .delete(`/tickets`)
+            .then(res => { 
+            setTickets(res.data)
+            console.log(res.data)
+            console.log(res)
+
+        })
+        .catch(err => {
+          console.log('err', err)
+        })
+    }
+
+  const randomNum = () => {
+    return Math.floor(Math.random() * 1000)
+  };
+
+    return (
+              <>
+            <Navbar />
+              {tickets &&
+                tickets.map(({ ticket_id, title, description, category, i }) => (
+
+                <Grid item xs={4} style={{ marginTop: '6rem' }}>
+                    <Card key={i} className={classes.card}>
+                        <CardContent>
+                          <CardHeader
+                            avatar={
+                              <Avatar
+                                alt="Profile Picture"
+                                src={"https://source.unsplash.com/daily"}
+                              />
+                            }
+                            title={`Subject: ${title}`}
+                            subheader={`Category: ${category}`}
+                          />
+                          <Divider />
+                        </CardContent>
+                        
+                        <CardActions>
+                        <Box
+                          borderColor="#00e676"
+                          border={1}
+                          borderRadius={100}
+                          >
+                        <IconButton              
+                          className={clsx(classes.expand, {
+                            [classes.expandOpen]: expanded,
+                          })}
+                          onClick={handleExpandClick}
+                        >
+                          
+                          <ExpandMoreIcon 
+                          color="action"
+                          />
+                        </IconButton>
+                          </Box>
+                          <Box
+                          borderColor="primary.main"
+                          border={1}
+                          borderRadius={5}
+                          >
+                          <Button
+                            varient="contained"
+                            size="small"
+                            color="primary"
+                            className={classes.button}
+                          >
+                            Claim ticket
+                          </Button>
+                          </Box>
+
+                          <Box 
+                          borderColor="secondary.main"
+                          border={1}
+                          borderRadius={5}
+                          >
+                          <Button
+                            onClick={deleteTicket}
+                            varient="contained"
+                            size="small"
+                            color="secondary"
+                            className={classes.button}
+                          >
+                            Close ticket
+                          </Button>
+                          </Box>
+                        </CardActions>
+                        <Collapse in={expanded} timeout="auto" unmountOnExit>
+                        
+                        <CardContent>
+                          <Typography paragraph>Description:</Typography>
+                          <Typography paragraph>{description}</Typography>
+                          </CardContent>
+                      </Collapse>
+                      </Card>
+                    </Grid>
+                ))}
+            </>
+          );
+        
+}
 
 const useStyles = makeStyles(theme => ({
-container: {
-    display: 'flex',
-    },
   text: {
     padding: theme.spacing(2, 2, 0),
   },
@@ -33,92 +168,24 @@ container: {
   button: {
     margin: theme.spacing(1),
   },
-
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
 }))
 
-export default function OpenTicket(props) {
-  const classes = useStyles()
-  const [tickets, setTickets] = useState([])
-
-  useEffect(() => {
-    axios()
-      .get('/users/1/tickets')
-      .then(res => {
-        setTickets(res.data)
-        console.log(res.data)
-      })
-      .catch(err => {
-        console.log('err', err)
-      })
-  }, [])
-
-// Delete ticket
-    const deleteTicket = () => {
-        axios()
-            .delete(`/tickets/${props.match.params.id}`)
-            .then(res => { 
-            setTickets(res.data)
-            console.log(res.data)
-        })
-        .catch(err => {
-          console.log('err', err)
-        })
-    }
-
-  const randomNum = () => {
-    return Math.floor(Math.random() * 1000)
-  };
-
-  return (
-    <Container>
-      {tickets &&
-        tickets.map(({ title, description, category }) => (
-          <Grid
-            container
-            spacing={2}
-            direction="row"
-            justify="center"
-            alignItems="center"
-          >
-            <Grid item lg={10}></Grid>
-            <Card className={classes.card}>
-              <CardContent>
-                <CardHeader
-                  avatar={
-                    <Avatar
-                      alt="Profile Picture"
-                      src={`https://source.unsplash.com/random/?people?sig="${randomNum()}`}
-                    />
-                  }
-                  title={title}
-                  subheader={category}
-                />
-
-                <Divider variant="inset" component="li" />
-                <Typography paragraph>{description}</Typography>
-              </CardContent>
-              <CardActions>
-                <Button
-                  varient="contained"
-                  size="small"
-                  color="primary"
-                  className={classes.button}
-                >
-                  Open ticket
-                </Button>
-                <Button
-                  onClick={deleteTicket}
-                  varient="contained"
-                  size="small"
-                  color="secondary"
-                  className={classes.button}
-                >
-                  Close ticket
-                </Button>
-              </CardActions>
-            </Card>
-          </Grid>
-        ))}
-    </Container>
-  )
-}
+// return (
+//   <>
+//     <Grid container direction="row" item xs={12} spacing={1} justify="center">
+//         <Grid container item xs={12} spacing={3}>
+//           <FormRow />
+//         </Grid>
+//     </Grid>
+//   </>
+//   )
--- devdesk/src/views/StudentDashboard.js
+++ devdesk/src/views/StudentDashboard.js
@@ -3,37 +3,85 @@ import { axiosWithAuth as axios } from '../utils/axiosConfig'
 
 import OpenTicket from '../components/OpenTicket'
 import Card from '@material-ui/core/Card'
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
+import { Link } from 'react-router-dom'
 
-export default function StudentDashboard() { 
+export default function StudentDashboard() {
+  const classes = useStyles()
   const [tickets, setTickets] = useState([])
+  const user_id = localStorage.getItem('user_id')
 
   useEffect(() => {
-    axios()
-    .get('/users/1/tickets')
-    .then(res => { setTickets(res.data)})
-    .catch(err => { console.log('err', err)
-    })
+    getAllTickets()
   }, [])
 
-    return (
-      <section>
-        <Card>
-        {tickets && tickets.map(ticket => {
-          return (
+ const getAllTickets = () => {
+    axios()
+      .get(`users/1/tickets/`)
+      .then(res => {
+        setTickets(res.data)
+      })
+      .catch(err => {
+        console.log('err', err)
+      })
+  }
+   {/* axios()
+      .get(`/tickets/${user_id}/tickets`)
+      .then(res => {
+        setTickets(res.data)
+      })
+      .catch(err => {
+        console.log('err', err)
+      }) */}
+  
+
+  return (
+    <Container component='main' maxWidth='lg' className={classes.paper}>
+      <Link to='/newticket' style={{ textDecoration: 'none' }}>
+      <Button
+        variant='contained'
+        color='secondary'
+        className={classes.buttons}
+      >
+        Create Ticket
+      </Button>
+      </Link>
+
+      <Card className={classes.paper}>
+        {tickets &&
+          tickets.map(ticket => {
+            return (
               <OpenTicket
-                ticket={ticket} 
+                ticket={ticket}
                 key={ticket.id}
                 title={ticket.title}
                 description={ticket.description}
                 category={ticket.category}
                 user_id={ticket.user_id}
-                created_at={ticket.created_at}
-                updated_at={ticket.updated_at}
-                />
+              />
             )
           })}
       </Card>
-      </section>
-      
-    )
-    }
+    </Container>
+  )
+}
+
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white,
+    },
+  },
+  paper: {
+    margin: theme.spacing(4, 0),
+    display: 'flex',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttons: {
+    margin: theme.spacing(2, 1),
+  },
+}))
